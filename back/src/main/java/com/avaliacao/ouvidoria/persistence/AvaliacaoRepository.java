25bad2c2b624b88b97e410e866ee7a54fca1d605
Matheus Oliveira <matheushso99@hotmail.com>
2021-05-25 22:01:00 -0300

    Implementando API da Avaliação

--- back/src/main/java/com/avaliacao/ouvidoria/persistence/AvaliacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.avaliacao.ouvidoria.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.avaliacao.ouvidoria.domain.model.Avaliacao;
-
-public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
-
-}
