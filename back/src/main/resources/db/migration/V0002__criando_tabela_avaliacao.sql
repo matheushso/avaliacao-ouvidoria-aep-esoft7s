a76c2de857087304af37b1a45be3c722c55336c0
Matheus Oliveira <matheushso99@hotmail.com>
2021-06-06 23:39:33 -0300

    Commit parcial do post da API Avaliacao

--- back/src/main/resources/db/migration/V0002__criando_tabela_avaliacao.sql
+++ back/src/main/resources/db/migration/V0002__criando_tabela_avaliacao.sql
@@ -2,7 +2,6 @@ create table avaliacao(
 	id bigint not null auto_increment,
 	protocolo int(10) not null,
     resposta int(2) not null,
-    setor varchar(50) not null,
     
     primary key(id)
 );
