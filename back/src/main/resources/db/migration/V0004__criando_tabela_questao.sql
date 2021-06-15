436ac0a611d5bc7624181a871a16619ff8d4587a
Matheus Oliveira <matheushso99@hotmail.com>
2021-06-15 01:06:48 -0300

    Criando API Questão

--- back/src/main/resources/db/migration/V0004__criando_tabela_questao.sql
+++ back/src/main/resources/db/migration/V0004__criando_tabela_questao.sql
@@ -1,7 +1,7 @@
 create table questao(
     id bigint not null auto_increment,
     pergunta int(2) not null,
-    questao varchar(150) not null,
+    questao varchar(250) not null,
 
     primary key(id)
 );
