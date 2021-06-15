a768578ee0ce43a579b506f94be1cadde80017b6
Matheus Oliveira <matheushso99@hotmail.com>
2021-06-15 01:05:44 -0300

    Criando API Questao

---
+++ back/src/main/java/com/avaliacao/ouvidoria/domain/model/Questao.java
@@ -0,0 +1,39 @@
+package com.avaliacao.ouvidoria.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "questao")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
+public class Questao {
+	
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Column
+	private int pergunta;
+	
+	@NotBlank
+	@Column
+	private String questao;
+	
+}
---
+++ back/src/main/java/com/avaliacao/ouvidoria/domain/service/QuestaoService.java
@@ -0,0 +1,24 @@
+package com.avaliacao.ouvidoria.domain.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.avaliacao.ouvidoria.domain.model.Questao;
+import com.avaliacao.ouvidoria.domain.repository.QuestaoRepository;
+
+@Service
+@Transactional
+public class QuestaoService {
+	
+	@Autowired
+	private QuestaoRepository repository;
+	
+	public List<Questao> findAll() {
+		return repository.findAll();
+	}
+	
+}
